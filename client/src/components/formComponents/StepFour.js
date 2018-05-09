82aa9068a883d65e7188d4d01ef03620ed6ee303
David Coulombe <David.Coulombe7@gmail.com>
2018-05-09 00:33:31 -0400

    some changes and saved dropdown

--- client/src/components/formComponents/StepFour.js
+++ client/src/components/formComponents/StepFour.js
@@ -2,28 +2,24 @@ import React from 'react';
 
 
 const StepFour = (props) => (
-    <div>
-        <br />
-        <br />
-        <p>First Name: {props.first_name}</p>
-        <p>Last Name: {props.last_name}</p>
-        <p>Email: {props.email}</p>
-        <p>Password: {props.password}</p>
-        <p>DOB: {props.dob}</p>
-        <p>Sex: {props.sex}</p>
-        <p>Height: {props.heightFoot}' {props.heightInch}"</p>
-        <p>Weight: {props.weight}</p>
-        <p>Waist Measurement: {props.waist}</p>
-        <p>Blood Pressure -- Systolic: {props.bp_systolic}</p>
-        <p>Blood Pressure -- Diastolic: {props.bp_diastolic}</p>
-        <p>Health Risk Factor: {props.risk_factor}</p>
-<<<<<<< HEAD
-        <p>Diet Recommendation: {props.dietRecommendation}</p>
-=======
-        <p>Diet Recommendation: {props.diet_recommendation}</p>
-        <p>Diet restriction: {props.diet_restriction}</p>
->>>>>>> b3b126210fd4fb78ebbf866b6a2a281f29ebae09
-    </div>
+        <div>
+                <br />
+                <br />
+                <p>First Name: {props.first_name}</p>
+                <p>Last Name: {props.last_name}</p>
+                <p>Email: {props.email}</p>
+                <p>Password: {props.password}</p>
+                <p>DOB: {props.dob}</p>
+                <p>Sex: {props.sex}</p>
+                <p>Height: {props.heightFoot}' {props.heightInch}"</p>
+                <p>Weight: {props.weight}</p>
+                <p>Waist Measurement: {props.waist}</p>
+                <p>Blood Pressure -- Systolic: {props.bp_systolic}</p>
+                <p>Blood Pressure -- Diastolic: {props.bp_diastolic}</p>
+                <p>Health Risk Factor: {props.risk_factor}</p>
+                <p>Diet Recommendation: {props.diet_recommendation}</p>
+                <p>Diet restriction: {props.diet_restriction}</p>
+        </div>
 )
 
 export default StepFour;
