7e3613e5c180a394083e7d9e9c1dbfb6de00e293
David Coulombe <David.Coulombe7@gmail.com>
2018-05-13 19:49:48 -0400

    doctor stuff

--- client/src/components/UserInfo/UserInfo.js
+++ client/src/components/UserInfo/UserInfo.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import { Panel, Button } from "react-bootstrap";
 import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import "./UserInfo.css";
 
 class UserInfo extends React.Component {
   state = {
@@ -60,29 +61,44 @@ class UserInfo extends React.Component {
             <div>
               <h5>
                 <Link to={`/doctor/${this.state.doctor_id}`}>
-                  <FontAwesomeIcon icon="list" /> Patient List
+                  <FontAwesomeIcon icon="list" /> My Patients
               </Link>
               </h5>
-              <dl>
-                <dt>Patient ID:</dt>
-                <dd>{this.state._id}</dd>
-                <dt>Name:</dt>
-                <dd>{this.state.first_name + " " + this.state.last_name}</dd>
-                <dt>Password:</dt>
-                <dd>{this.state.password}</dd>
-                <dt>Risk Factor:</dt>
-                <dd>{this.state.risk_factor}</dd>
-                <dt>Diet Recommendation:</dt>
-                <dd>{this.state.diet_recommendation}</dd>
-                <dt>Diet Restriction:</dt>
-                <dd>{this.state.diet_restriction}</dd>
-              </dl>
+
+
+              <div className="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
+              <h2>{this.state.first_name + " " + this.state.last_name}</h2>
+              <img className="thumbnail user-thumb col-xs-12 col-sm-12 col-md-3 col-lg-3" src={this.state.user_photo} alt={this.state.first_name} />
+              <div className="col-xs-12 col-sm-12 col-md-7 col-lg-7">
+                <div className="row">
+                  <div className="col-xs-12 col-sm-12 col-md-1 col-lg-1">
+                  </div>
+                  <div className="col-xs-12 col-sm-12 col-md-5 col-lg-5">
+                    <h3>{this.state.email}</h3>
+                    <h4>Risk Factor:{" "}{this.state.risk_factor}</h4>
+                    <h4>Height:{" "}{this.state.heightFoot}'{this.state.heightInch}"</h4>
+                    <h4>Weight:{" "}{this.state.weight}</h4>
+                    <h4>Waist Measure:{" "}{this.state.waist}</h4>
+                  </div>
+                  <div className="col-xs-12 col-sm-12 col-md-5 col-lg-5">
+                    <h4>DOB:{" "}{this.state.dob}</h4>
+                    <h4>Diet Recommendation:{" "}{this.state.diet_recommendation}</h4>
+                    <h4>Diet Restriction:{" "}{this.state.diet_restriction}</h4>
+                    <h4>Systolic BP:{" "}{this.state.bp_systolic}</h4>
+                    <h4>Diastolic BP:{" "}{this.state.bp_diastolic}</h4>
+                  </div>
+                  <div className="col-xs-12 col-sm-12 col-md-1 col-lg-1">
+                  </div>
+                </div>
+              </div>
+
+
               <Link to={{ pathname: `/edit/${this.state._id}`, params: { data: this.state, doctor_id: this.props.location.params.doctor_id } }} className="btn btn-success">Edit</Link>&nbsp;
               <Button id={this.state._id} onClick={this.deletePatient.bind(this)} color="danger">Delete</Button>
             </div>
           </Panel.Body>
         </Panel>
-      </div>
+      </div >
     )
   }
 }
