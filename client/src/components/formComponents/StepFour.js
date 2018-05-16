389aeb66dfcbf3e89b3bbe763d2aa70d9906c14b
David Coulombe <David.Coulombe7@gmail.com>
2018-05-16 16:32:00 -0400

    final push pre build

--- client/src/components/formComponents/StepFour.js
+++ client/src/components/formComponents/StepFour.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Moment from 'moment';
 
 
 const StepFour = (props) => (
@@ -10,7 +11,7 @@ const StepFour = (props) => (
                 <h5 className="review-info">Email:{" "}{props.email}</h5>
                 <h5 className="review-info">Password:{" "} ****************************</h5>
                 <h5 className="review-info">IMG:{" "} {props.user_photo}</h5>
-                <h5 className="review-info">DOB: {" "}{props.dob}</h5>
+                <h5 className="review-info">DOB: {" "}{Moment(props.dob).format('MMMM Do YYYY')}</h5>
                 <h5 className="review-info">Sex:{" "}{props.sex}</h5>
                 <h5 className="review-info">Height: {" "}{props.heightFoot}' {props.heightInch}"</h5>
                 <h5 className="review-info">Weight:{" "}{props.weight}</h5>
@@ -20,7 +21,7 @@ const StepFour = (props) => (
                 <h5 className="review-info">Health Risk Factor: {" "}{props.risk_factor}</h5>
                 <h5 className="review-info">Diet Recommendation: {" "}{props.diet_recommendation}</h5>
                 <h5 className="review-info"> Diet restriction: {" "}{props.diet_restriction}</h5>
-        </div >
+        </div>
 )
 
 export default StepFour;
